3edb1a79e55579c136fb6d6f20c10e0e3b8b6724
yuenanmu <1730593502@qq.com>
2025-07-05 20:09:56 +0800

    电机保护逻辑加入，电机转向控制还在想怎么优化

--- Ds.sr_MM32/project/code/motor.h
+++ Ds.sr_MM32/project/code/motor.h
@@ -3,5 +3,19 @@
 
 #include "zf_common_headfile.h"
 
+typedef struct
+{
+	float speed;
+	float speed_L;
+	float speed_R;
+	
+    float L_Ki;    
+	float L_Kp;  
+	float R_Ki;  
+	float R_Kp;
+
+	float Dif_P;  //Differential Proportional
+	float Dif_Speed;  
+}MOTOR_PID;
 
 #endif
